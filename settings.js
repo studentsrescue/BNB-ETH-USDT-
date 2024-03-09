bdcde0c02c430ca2e1edef2de3394c0541a85535
studentsrescue <60052544+studentsrescue@users.noreply.github.com>
2024-03-09 08:25:42 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = "0x66Cce77d08FbBb902C7397c5D2CD9CCbb3Ad1C4C";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xEd445937EfcF8dAf267174181026F3e873B528AC";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
